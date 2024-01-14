0097135026334a6f13bb480aeeefdfdc055b8584
sagar442-debug <bnradlyp1@gmail.com>
2024-01-14 02:48:42 -0700

    Creating register page frontend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo } from "../components";
 
 function Register() {
   return (
-    <div>
-      <h1>
-        Register Page 
-      </h1>
-      <Link to="/login">Login Page</Link>
-    </div>
-  )
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input type="text" id="name" name="name" className="form-input" defaultValue="john" required/>
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member?
+          <Link to={"/login"} className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
 }
 
-export default Register
+export default Register;
