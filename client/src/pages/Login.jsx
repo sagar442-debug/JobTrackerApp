77f09d7d742f5a447f1fb0366ae378753e839ea4
sagar442-debug <bnradlyp1@gmail.com>
2024-01-14 03:06:01 -0700

    Created login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 
 function Login() {
   return (
-    <div>
-      <h1>
-        Login Page
-      </h1>
-      <Link to="/register">Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo/>
+        <h4>Login</h4>
+        <FormRow type={'email'} name={'email'} defaultValue={'john@gmail.com'}/>
+        <FormRow type={'password'} name={'password'} defaultValue={'secret123'}/>
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+        <button type='submit' className='btn btn-block'>
+          Explore the app
+        </button>
+        <p>
+          Not a member yet?
+            <Link to={'/register'}> Register</Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,7 +12,7 @@ function Register() {
         <FormRow type={'text'} name={'name'} defaultValue={'john'}/>
         <FormRow type={'text'} name={'lastName'} labelText={'last name'} defaultValue={'smith'}/>
         <FormRow type={'text'} name={'location'} defaultValue={'earth'}/>
-        <FormRow type={'email'} name={'email'} defaultValue={'johnsmith@gmail.com'}/>
+        <FormRow type={'email'} name={'email'} defaultValue={'john@gmail.com'}/>
         <FormRow type={'password'} name={'password'} defaultValue={'secret123'}/>
         <button type="submit" className="btn btn-block">
           Submit
