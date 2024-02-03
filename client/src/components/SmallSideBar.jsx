2dc4b673f5241eac6d400781644721fd4f4cc3db
sagar442-debug <bnradlyp1@gmail.com>
2024-02-03 00:00:21 -0700

    Creating sidebar functionality

--- client/src/components/SmallSideBar.jsx
+++ client/src/components/SmallSideBar.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import Wrapper from '../assets/wrappers/SmallSidebar'
-import { useDashboardContext } from '../pages/DashboardLayout'
-
-
+import React from "react";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { useDashboardContext } from "../pages/DashboardLayout";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import { NavLink } from "react-router-dom";
+import links from "../utils/Links";
 const SmallSideBar = () => {
-  const {showSidebar} = useDashboardContext();
-
-  console.log(showSidebar)
+  const { showSidebar } = useDashboardContext();
 
   return (
     <Wrapper>
-      Small Side Bar
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type="button" className="close-btn">
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map((link) => {
+              const { text, path, icon } = link;
+              return (
+                <NavLink to={path} key={text} className="nav-link">
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default SmallSideBar
+export default SmallSideBar;
